34661f2c239be27b6b4d5a0cd074aff079c4cdae
burakulas <35465277+burakulas@users.noreply.github.com>
2018-01-16 14:20:55 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,34 @@
+ 
+
+LCSPLIT is a short bash script for splitting long two-column data into several parts based on given period value. The original aim of the script is to split long astronomical data sets like Kepler light curve data of binary stars into several sub-light curves to investigate them separately. However, technically, it can be applied all time related two-column data by entering the appropriate period value. The code first sorts the long data and then split it according to period entered by the user. It also asks minimum desired number which allows the user to select the sub-light curves having enough data points that are generally suitable for further analysis. This could be useful, for instance, when splitting a whole binary light curve into hundreds of sub-light curves to analyze and investigate them one-by-one.
+
+- HOW TO RUN
+The user is advised to put the two-column data and the script (lcsplit.sh) in the same directory. First, set the permission by typing:
+
+chmod a+x lcsplit.sh
+
+with the root privileges. Then run the script with:
+
+./lcsplit.sh
+
+in bash.
+
+Depending on your entries, the sub-lightcurves are extracted in a folder named LightCurves created by the script.
+
+The script asks the user:
+
+1) Name of the data file, when it expects that the user enters the name of the data file and hit the enter.
+2) Period value, the value which bases the splitting process. The script cuts long data according to this value and it must be greater than zero.
+3) The minimum desired number of data points in each sub-light curve; the script ignores the sub-light curves which do not contain the number of data points smaller than this value. Therefore, small values allow the script to create more sub-light curves.
+
+- NOTES:
+1) .tpl file extension is used by the script for the temporary lists and .scl file extension is used for sub-light curves. Therefore, it is adviced not to use these extensions as your data file extension to avoid it from removing or overwriting at the end of the run.
+2) Re-running the script warns the user about removing the old sub-light curve folder. Take a backup or change the folder name if you believe that you need it.
+3) The script runs without any problem in the following Linux distributions:
+   . Ubuntu 16.04, 17.10
+   . Fedora ...
+
+When used you may want to refer the script by giving its GitHub address as the reference.
+
+For comments and further questions: bulash@gmail.com
+
---
+++ lcsplit.sh
@@ -0,0 +1,285 @@
+#!/bin/bash
+
+##### LC-SPANS (Lightcurve Split and Analyse Script) (c)2018 Burak Ulas ####
+
+clear
+
+echo -e "\033[1;31m  _    ___  ___ ___ _    ___ _____ \033[0m"
+echo -e "\033[1;31m | |  / __|/ __| _ \ |  |_ _|_   _|\033[0m"
+echo -e "\033[1;31m | |_| (__ \__ \  _/ |__ | |  | |  \033[0m"
+echo -e "\033[1;31m |____\___||___/_| |____|___| |_|  \033[0m"
+echo -e "\033[1;31m ________________________________\033[0m"
+echo " "
+
+ls > lslist.tpl
+
+ls1=$(cat lslist.tpl | awk '{if ($1 == "work") print "1";}')
+if [[ $ls1 -eq  1 ]]; then
+      read -r -p $'Program detected the \e[0;32mwork\e[0m folder from previous run. It will be removed. \e[1;33mContinue? [Y/n] \e[0m: ' che1
+      case $che1 in
+    	 [yY][eE][sS]|[yY])
+ 	 echo "Yes"
+ 	 rm -rf work
+ 	 ;;
+    	    [nN][oO]|[nN]|*)
+ 	 echo "No"
+ 	 exit 1    ;;
+    	    *)
+ 	 echo "Invalid input..."
+ 	 exit 1
+ 	 ;;
+       esac
+fi
+
+ls3=$(cat lslist.tpl | awk '{if ($1 == "LightCurves") print "3";}')
+if [[ $ls3 -eq  3 ]]; then
+      read -r -p $'Program detected the \e[0;32mLightCurves\e[0m folder from previous run. It will be removed. \e[1;33mContinue? [Y/n] \e[0m: ' che3
+      case $che3 in
+         [yY][eE][sS]|[yY])
+         echo "Yes"
+         rm -rf LightCurves
+         ;;
+            [nN][oO]|[nN]|*)
+         echo "No"
+         exit 1    ;;
+            *)
+         echo "Invalid input..."
+         exit 1
+         ;;
+       esac
+fi
+
+read -p $'\e[1;33mName of the data file\e[0m : ' df
+if [ ! -f $df ]; then
+    echo "File not found!"
+    exit 1
+fi
+
+read -p $'\e[1;33mEnter the period\e[0m : ' p
+re='^[0-9]+([.][0-9]+)?$'
+if ! [[ $p =~ $re ]] ; then
+   echo "Period value is not valid." >&2; exit 1
+fi
+
+
+echo  Sorting the light curve...
+
+sort_1(){
+sort -k1 $df -o temp0.tpl
+mv temp0.tpl $df 
+}
+export -f sort_1
+
+
+sort_sp0(){
+sort_1&
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
+}
+
+export -f sort_sp0
+
+sort_sp0
+
+to=$(awk 'FNR == 1 {print $1}' $df)
+
+echo " "
+echo Preparing the light curve for splitting...
+cp $df tabl.tpl
+sp0(){
+
+awk -v a="$to" '{print $1,$2,a}' tabl.tpl > inx.tpl
+awk -v b="$p" '{print $1,$2,$3,b}' inx.tpl > in2x.tpl
+awk '{print $1,$2,int(($1-$3)/$4)}' in2x.tpl > alltocode.tpl
+}
+export -f sp0
+
+sp_sp0(){
+sp0 &
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
+}
+
+export -f sp_sp0
+sp_sp0
+
+#####
+
+awk '{printf"%-10s %-8s %-0s\n",$1,$2,$3}' alltocode.tpl >  allcodec2.tpl
+#cat allcodec2.tpl | awk '{if(min==""){min=max=$3}; if($3>max) {max=$3}} END {print max}' > allcodemax.tpl
+#awk '{printf"%0s",$1}' allcodemax.tpl > temp1.tpl
+#NUM1=$(awk '{printf $1+1}' temp1.tpl)
+#NUM2=$(awk '{printf $1}' temp1.tpl)
+#seq 0 $NUM2 > numtab0.tpl
+#seq -w 0000  $NUM2 > numtab1.tpl
+#seq -w 0001 $NUM1 > numtab2.tpl
+
+echo -e "\033[1;31mSplitting process starts from the first datapoint.\033[0m"
+echo Splitting started...
+
+split_1(){
+#pr -mts numtab0.tpl numtab1.tpl numtab2.tpl > numtab.tpl
+awk '{printf"%-0s,%-0s,%-0s\n",$1,$2,$3}' allcodec2.tpl >  allcodecom.tpl
+awk -F, '{print > "data"$3}' allcodecom.tpl
+
+}
+
+export -f split_1
+mkdir work
+don(){
+split_1
+ls data* > datlist.tpl
+datlist=$(cat datlist.tpl | wc -l)
+echo -e Light curve is split to "\033[1;34m$datlist\033[0m" sub-light curves.
+
+}
+export -f don
+
+sp_sp(){
+don &
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
+}
+export -f sp_sp
+
+sp_sp
+
+printf "\n"
+read -p $'\e[1;33mEnter the desired min. number of datapoints in each sub-light curve file\e[0m: ' mnd
+echo -e "\033[1;31mSub-light curves with smaller than\033[0m" "\033[1;34m$mnd\033[0m" "\033[1;31mdatapoints will be ignored.\033[0m"
+if_1(){
+ls data* > datlist.tpl
+datlist=$(cat datlist.tpl | wc -l)
+for i in data*;do
+    kar=$(cat $i | wc -l)
+    if [[ $mnd -le kar ]]
+       then
+            printf "$i\n" >> templist2a.tpl
+	    tmpla=$(cat templist2a.tpl | wc -l) 
+	   continue
+       else
+            	printf "$i\n" > templistb$i.tpl
+            	cat templistb* > templist2b.tpl
+                tmplb=$(cat templist2b.tpl | wc -l)
+		if [[ $tmplb = $datlist ]]
+    			then
+				printf "$i\n" >> templist2b2.tpl
+    			else
+				printf "$i\n" >> templistc.tpl
+		fi
+    fi
+done
+}
+
+export -f if_1
+
+
+if_1_sp(){
+if_1 &
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
+}
+export -f if_1_sp
+
+if_1_sp
+
+
+
+
+
+if [ ! -f templist2b2.tpl ]
+	then
+		if [ ! -f templistc.tpl ]
+                        then
+                                echo -e "\033[1;34m0\033[0m" sub-light curves are ignored.
+                                datlist=$(cat datlist.tpl | wc -l)
+				echo -e Proceeding with "\033[1;34m$datlist\033[0m" sub-light curves...
+			else
+                		iglist=$(cat templistc.tpl | wc -l)
+                		tmpla=$(cat templist2a.tpl | wc -l)
+				xargs rm -r < templistc.tpl
+                		echo -e "\033[1;34m$iglist\033[0m" sub-light curves are ignored.
+                		echo -e Proceeding with "\033[1;34m$tmpla\033[0m" sub-light curves...
+		fi
+	else
+                rm -rf work
+		rm dat*
+		rm *.tpl
+		echo " "
+                echo None of the sub-light curves contain the desired min. number of datapoints. All curves are ignored.
+                echo Re-run the script and enter smaller number of datapoints in related prompt or check your period value.
+                echo -e "\033[0;31mProgram stopped.\033[0m"
+                echo " "
+		exit 1
+fi
+
+
+
+for_1(){
+for i in data*;do
+	cat $i > dt.tpl 2> /dev/null
+	sed -i 's/,/ /g' dt.tpl
+	awk '{printf"%-0s %-0s\n",$1,$2}' dt.tpl >  $i.slc
+	rm dt.tpl
+done
+}
+
+export -f for_1
+
+for_1_sp(){
+for_1 &
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
+}
+
+export -f for_1_sp
+
+for_1_sp
+
+mkdir LightCurves
+
+for file in data*;do
+postfile=${file#data}
+number=${postfile%.slc}
+i=$((number-0))
+mv ${file} $(printf data%06d.slc $i)
+done
+mv *.slc ./LightCurves/
+
+
+
+mv dat* ./work
+mv *.tpl ./work
+rm -rf work
+echo -e Splitting finished. Sub-light curves are extracted to "\e[0;32mLightCurves\e[0m" folder.
+echo " "
